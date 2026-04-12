a1d7351ebf1c184d80c21b3a5e3597b5a6ca4adb
Leif Josh Flores <floresleifjosh@gmail.com>
2026-04-11 23:46:26 -0400

    Update Product.h

--- include/Product.h
+++ include/Product.h
@@ -4,9 +4,22 @@
 #include <string>
 using namespace std;
 
+/*
+  Product class
+  It stores the basic information for each item, such as name, price, and quantity 
+*/
+
 class Product {
 private:
+  char m_name[100]; //Item name (Product)
+  double m_price; //store the price of the item
+  int m_quantity; //stores the available quantity of the item
 
 public:
+  Product(); //Default constructor
+
+  // Initializes product with given name, price, and quantity
+  Product(const char* name, double price, int quantity);
+
 };
 #endif
