5e35d1767c3f668543e5f29b8bb7ec948ad4d5cc
Alex Le <alexle16@hotmail.com>
2026-04-11 23:37:00 -0400

    Enhance CustomerInterface with menu and cart methods
    Added methods for menu handling and cart management.

--- include/CustomerInterface.h
+++ include/CustomerInterface.h
@@ -1,15 +1,20 @@
 #ifndef CUSTOMERINTERFACE_H
 #define CUSTOMERINTERFACE_H
-
 #include "Interface.h"
 #include "Cart.h"
 
-class CustomerInterface: public Interface {
+class CustomerInterface: public Interface 
+{
 private:
-
+  Cart cart;
 
 public:
+  void showMenu();
+  void handleMenu();
 
+private:
+  void showCartMenu();
+  void browseDepartments();
 
 };
 
