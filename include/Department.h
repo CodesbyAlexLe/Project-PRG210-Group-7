cfa5f72996aa5e65aaeeaa1489342fb6fa73007a
Leif Josh Flores <floresleifjosh@gmail.com>
2026-04-11 23:39:44 -0400

    Update Department.h

--- include/Department.h
+++ include/Department.h
@@ -4,12 +4,17 @@
 #include "Product.h"
 using namespace std;
 
+//Department class stores multiple products
 class Department {
 private:
-
+  char departmentName[100]; //name of department
+  Product* items;
+  int totalItems; //Number of items in department
 
 public:
-
+  //Constructors
+     Department();
+     Department(const char* name); 
 };
 
 #endif
