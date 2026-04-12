5a30d8e0cbef13233e91885466f0e2dc2c53d838
Leif Josh Flores <floresleifjosh@gmail.com>
2026-04-11 23:42:05 -0400

    Update Interface.h

--- include/Interface.h
+++ include/Interface.h
@@ -1,6 +1,13 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
+#include "Department.h"
+#include "Cart.h"
+
+/*
+    interface base class for both manager and customer interfaces
+    demonstrates inheritance + polymorphism
+ */
 class Interface {
 public:
 
