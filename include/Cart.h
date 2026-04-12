7dd6e63e5cb5a8698add54ec38404ddcf1b4b731
Alex Le <alexle16@hotmail.com>
2026-04-11 23:34:49 -0400

    Define Cart class with item management methods
    Added member variables and methods for managing cart items.

--- include/Cart.h
+++ include/Cart.h
@@ -1,15 +1,25 @@
 #ifndef CART_H
 #define CART_H
-
 #include "Product.h."
 
-class Cart {
-
+class Cart 
+{
 private:
-
+  Product* cartItems;
+  int* quantities;
+  int totalItems;
+  double totalCost;
 
 public:
+  Cart();
+  ~Cart();
 
+  void addToCart(const Product& p, int quantity);
+  void listCartItems() const;
+  void checkout();
+  bool isEmpty() const;
+  void clearCart();
+  double calculateTotal() const;
 };
 
 #endif
