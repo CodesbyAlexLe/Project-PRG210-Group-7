2d406c41530fa6578d52360d7083e45fc055d4a5
Jasraj Chhokar <jchhokar2@myseneca.ca>
2026-04-12 23:24:02 -0400

    Update Interface.h

--- include/Interface.h
+++ include/Interface.h
@@ -1,34 +1,22 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
-
 #include "Department.h"
 #include "Cart.h"
-
 /*
-    interface base class for both manager and customer interfaces
-    demonstrates inheritance + polymorphism
-        - Inheritance (Manager interface and customer interface inherit from this)
-        - Polymorphism 
- */
+interface base class for both manager and customer interfaces
+demonstrates inheritance + polymorphism
+*/
 class Interface {
 public:
     virtual void showMenu() = 0; //Displays the menu
-    virtual void handleMenu() = 0; // handles users' input and performs actions based on menu choice
-    virtual ~Interface(); //Destructor 
-    virtual void run() = 0;
-    virtual ~Interface() {}
-
+    virtual void handleMenu() = 0; // handles user input and performs actions based on menu choice
+    virtual ~Interface(); //Destructor
 
 protected:
-    int getValidatedInt(int min, int max); //Validates integer input within a given range 
+    int getValidatedInt(int min, int max); //Validates integer input within a given range
     int getValidatedPositiveInt();
     double getValidatedPositiveDouble();
     void getValidatedNonEmptyString(char* str, int size);
- 
-protected: 
-    int getValidInt(int min, int max) const;
-    double getValidDouble(double min) const;
-    void getValidString(char* buffer, int size) const; 
 };
 
 #endif
