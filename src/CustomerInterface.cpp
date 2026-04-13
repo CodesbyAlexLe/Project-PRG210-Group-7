d3e01ed9fcd51b5f8cecd8b7e001f3aebf8cf779
Jasraj Chhokar <jchhokar2@myseneca.ca>
2026-04-12 21:02:39 -0400

    Update CustomerInterface.cpp
    Fixed typos causing incorrect department and item selection

--- src/CustomerInterface.cpp
+++ src/CustomerInterface.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include "CustomerInterface.h"
 #include "Department.h"
@@ -7,170 +8,178 @@ extern int TotalDepartments;
 
 void CustomerInterface::showMenu()
 {
-  cout << "\nCustomer Menu:" << endl;
-  cout << "1. Show Cart" << endl;
-  cout << "2. Browse Departments" << endl;
-  cout << "3. Exit" << endl;
-  cout << "Enter your choice [1, 2, 3]: ";
-]
+    cout << "\nCustomer Menu:" << endl;
+    cout << "1. Show Cart" << endl;
+    cout << "2. Browse Departments" << endl;
+    cout << "3. Exit" << endl;
+    cout << "Enter your choice [1, 2, 3]: ";
+}
 
 void CustomerInterface::handleMenu()
 {
-  int choice;
+    int choice;
 
-  do
-{
-  showMenu();
-  choice = getValidatedInt(1, 2);
+    do
+    {
+        showMenu();
+        choice = getValidatedInt(1, 3);
 
-  switch (choice)
-{
-case 1:
-  showCartMenu();
-  break;
-case 2:
-  browseDepartments();
-  break;
-case 3:
-  cout << "Exiting customer interface..." << endl;
-  break;
-}
-} while (choice != 3);
+        switch (choice)
+        {
+        case 1:
+            showCartMenu();
+            break;
+        case 2:
+            browseDepartments();
+            break;
+        case 3:
+            cout << "Exiting customer interface..." << endl;
+            break;
+        }
+    } while (choice != 3);
 }
 
 void CustomerInterface::showCartMenu()
 {
-  int choice;
+    int choice;
 
-  do
-{
-  cout << "\nShow Cart Menu" << endl;
-  cout << "1. List Items" << endl;
-  cout << "2. Checkout" << endl;
-  cout << "3. Return to Main Menu" << endl;
-  cout << "Enter your choice [1, 2, 3]: ";
+    do
+    {
+        cout << "\nShow Cart Menu" << endl;
+        cout << "1. List Items" << endl;
+        cout << "2. Checkout" << endl;
+        cout << "3. Return to Main Menu" << endl;
+        cout << "Enter your choice [1, 2, 3]: ";
 
-  choice = getValidatedInt(1, 3);
+        choice = getValidatedInt(1, 3);
 
-  switch (choice)
-{
-case 1:
-  cart.listCartItems();
-  break;
-case 2:
-  cart.checkout();
-  break;
-case 3:
-  break;
-}
-} while (choice != 3);
+        switch (choice)
+        {
+        case 1:
+            cart.listCartItems();
+            break;
+        case 2:
+            cart.checkout();
+            break;
+        case 3:
+            break;
+        }
+    } while (choice != 3);
 }
 
 void CustomerInterface::browseDepartments()
 {
-  if (TotalDepartments == 0)
-{
-  cout << "No departments available." << endl;
-  return;
-}
+    if (TotalDepartments == 0)
+    {
+        cout << "No departments available." << endl;
+        return;
+    }
 
-for (int i = 0; i < TotalDepartments; i++)
-{
-  cout << i + 1 << ". " << StoreDepartments[i].getDepartmentName() << endl;
-}
+    for (int i = 0; i < TotalDepartments; i++)
+    {
+        cout << i + 1 << ". " << StoreDepartments[i].getDepartmentName() << endl;
+    }
 
-cout << "\n1. List Items of a Department" << endl;
-cout << "2. Go Back to Main Menu" << endl;
-cout << "Enter your choice [1, 2]: ";
+    cout << "\n1. List Items of a Department" << endl;
+    cout << "2. Go Back to Main Menu" << endl;
+    cout << "Enter your choice [1, 2]: ";
 
-int choice = getValidatedInt(1, 2);
+    int choice = getValidatedInt(1, 2);
 
-if (choice == 2)
-{
-  return;
-}
+    if (choice == 2)
+    {
+        return;
+    }
 
-cout << "Enter department number [0 to go back]: ";
+    cout << "Enter department number [0 to go back]: ";
 
-int deptChoice;
+    int deptChoice;
 
-while (true)
-{
-  cin >> deptChoice;
+    while (true)
+    {
+        cin >> deptChoice;
 
-  if (cin.fail())
-{
-  cin.clear();
-  cin.ignore(1000, '\n');
-  cout << "Invalid input. Enter department number [0 to go back]: ";
-}
-else if (deptChoice == 0)
-{
-  cin.ignore(1000, '\n');
-  cout << "Invalid department number. Enter department number [0 to go back]: ";
-}
-else
-{
-  cin.ignore(1000, '\n');
-  break;
-}
-}
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Invalid input. Enter department number [0 to go back]: ";
+        }
+        else if (deptChoice == 0)
+        {
+            cin.ignore(1000, '\n');
+            return;
+        }
+        else if (deptChoice < 1 || deptChoice > TotalDepartments)
+        {
+            cin.ignore(1000, '\n');
+            cout << "Invalid department number. Enter department number [0 to go back]: ";
+        }
+        else
+        {
+            cin.ignore(1000, '\n');
+            break;
+        }
+    }
 
-int deptIndex = deptChoice - 1;
+    int deptIndex = deptChoice - 1;
 
-if StoreDepartments[deptIndex].getTotalItems() == 0)
-{
-  return;
-}
+    if (StoreDepartments[deptIndex].getTotalItems() == 0)
+    {
+        cout << "No items in this department." << endl;
+        return;
+    }
 
-cout << "\n1. Add to Cart an Item" << endl;
-cout << "2. Go Back to Browse Departments Menu" << endl;
-cout << "Enter your choice [1, 2]: ";
+    StoreDepartments[deptIndex].listItems();
 
-int itemMenuChoice = getValidatedInt(1, 2);
+    cout << "\n1. Add to Cart an Item" << endl;
+    cout << "2. Go Back to Browse Departments Menu" << endl;
+    cout << "Enter your choice [1, 2]: ";
 
-if (itemMenuChoice == 2)
-{
-  return;
-}
+    int itemMenuChoice = getValidatedInt(1, 2);
 
-cout << "Enter item number to buy [0 ton go back]: ";
+    if (itemMenuChoice == 2)
+    {
+        return;
+    }
 
-int itemChoice;
-int totalItems = StoreDepartments[deptIndex].getTotalItems();
+    cout << "Enter item number to buy [0 to go back]: ";
 
-while (true)
-{
-  cin >> itemChoice;
+    int itemChoice;
+    int totalItems = StoreDepartments[deptIndex].getTotalItems();
 
-  if (cin.fail())
-{
-  cin.clear();
-  cin.ignore(1000, '\n');
-  cout << "Invalid input. Enter item number to buy [0 to go back]: ";
-}
-else if (itemChoice == 0)
-{
-  cin.ignore(1000, '\n');
-  return;
-}
-else if (itemChoice < 1 || itemChoice > totalItems)
-{
-  cin.ignore(1000, '\n');
-  cout << "Invalid item number. Enter item number to buy [0 to go back]: ";
-}
-else
-{
-  cin.ignore(1000, '\n');
-  break;
-}
-}
+    while (true)
+    {
+        cin >> itemChoice;
 
-cout << "Enter quantity to buy: ";
-int quantity = getValidatedPositiveInt();
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Invalid input. Enter item number to buy [0 to go back]: ";
+        }
+        else if (itemChoice == 0)
+        {
+            cin.ignore(1000, '\n');
+            return;
+        }
+        else if (itemChoice < 1 || itemChoice > totalItems)
+        {
+            cin.ignore(1000, '\n');
+            cout << "Invalid item number. Enter item number to buy [0 to go back]: ";
+        }
+        else
+        {
+            cin.ignore(1000, '\n');
+            break;
+        }
+    }
 
-Product selectedProduct = StoreDepartments[deptIndex].getItems()[itemChoice - 1];
-cart.addToCart(selectProduct, quantity);
+    cout << "Enter quantity to buy: ";
+    int quantity = getValidatedPositiveInt();
 
-cout << "Item added to cart successfully. " << endl;
+    Product selectedProduct = StoreDepartments[deptIndex].getItems()[itemChoice - 1];
+    cart.addToCart(selectedProduct, quantity);
+
+    cout << "Item added to cart successfully." << endl;
 }
