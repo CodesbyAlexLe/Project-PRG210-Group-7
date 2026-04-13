73072ecb5ebc4c85d36db12604c7c0538e0a4718
Jasraj Chhokar <jchhokar2@myseneca.ca>
2026-04-12 20:58:46 -0400

    Update Interface.cpp
    Fixing type errors and syntax bugs in validation methods

--- src/Interface.cpp
+++ src/Interface.cpp
@@ -1,89 +1,84 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <limits>
 #include <cstring>
 #include "Interface.h"
 using namespace std;
-
 Interface::~Interface()
 {
 }
 
-int Interface::getValidatedInt(int min, in max)
+int Interface::getValidatedInt(int min, int max)
 {
-  int choice;
-  while (true)
-{
-  cin >> choice;
-  if (cin.fail() || choice < min || choice > max)
-{
-  cin.clear();
-  cin.ignore(numeric_limits<streamsize>::max(), '\n');
-  cout << "Invalid input. Try again: ";
+    int choice;
+    while (true)
+    {
+        cin >> choice;
+        if (cin.fail() || choice < min || choice > max)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input. Try again: ";
+        }
+        else
+        {
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            return choice;
+        }
+    }
 }
-else
-{
-  cin.ignore(numeric_limits<streamsize>::max(), '\n');
-  return choice;
-}
-}
-}
-
 int Interface::getValidatedPositiveInt()
 {
-  int value;
-
-  while (true)
-{
-  cin >> value;
-  if (cin.fail() || value <= 0)
-{
-  cin.clear();
-  cin.ignore(numeric_limits<streamsize>::max(), '\n');
-  cout << "Invalid input. Enter a positive interger: ";
+    int value;
+    while (true)
+    {
+        cin >> value;
+        if (cin.fail() || value <= 0)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input. Enter a positive integer: ";
+        }
+        else
+        {
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            return value;
+        }
+    }
 }
-else
-{
-cin.ignore(numeric_limits<streamsize>::max(), '\n');
-return value;
-}
-}
-}
-
 double Interface::getValidatedPositiveDouble()
 {
     double value;
-
-  while (true)
-{
-  cin >> value;
-
-  if (cin.fail() || value <= 0)
-{
-  cin.clear();
-  cin.ignore(numeric_limits<streamsize>::max(), '\n');
-  cout << "Invalid input. Enter a positive number: ";
+    while (true)
+    {
+        cin >> value;
+        if (cin.fail() || value <= 0)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input. Enter a positive number: ";
+        }
+        else
+        {
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            return value;
+        }
+    }
 }
-else
-{
-  cin.ignore(numeric_limits<streamsize>::max(), '\n');
-  return value;
-}
-}
-}
-
 void Interface::getValidatedNonEmptyString(char* str, int size)
 {
-  while (true)
-{
-  cin.getline(str, size);
+    while (true)
+    {
+        cin.getline(str, size);
+        if (strlen(str) == 0)
+        {
+            cout << "Input cannot be empty. Try again: ";
+        }
+        else
+        {
+            return;
+        }
+    }
+}
 
-  if (strlen(str) == 0)
-{
-  cout << "Input cannot be empty. Try again: ";
-}
-else
-{
-  return;
-}
-}
 }
