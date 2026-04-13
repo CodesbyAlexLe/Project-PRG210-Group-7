ab240367add203fa63c20081d16d3de3194f080c
Leif Josh Flores <floresleifjosh@gmail.com>
2026-04-12 21:21:13 -0400

    Update Cart.h

--- include/Cart.h
+++ include/Cart.h
@@ -2,17 +2,22 @@
 #define CART_H
 #include "Product.h"
 
+    /*
+        Cart class
+        This class represents the customer's shopping cart
+        It stores selected products along with their quantities
+    */
 class Cart
 {
 private:
-    Product* cartItems;
+    Product* cartItems; 
     int* quantities;
-    int totalItems;
-    double totalCost;
+    int totalItems; //Total number of different items in the cart
+    double totalCost; //Total cost of items in the cart (including the tax)
 
 public:
-    Cart();
-    ~Cart();
+    Cart(); //Constructor
+    ~Cart(); //Destructor - Frees dynamically allocated memory (cartItems and quantities)
 
     void addToCart(const Product& p, int quantity);
     void listCartItems() const;
