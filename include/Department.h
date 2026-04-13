a007c943e6fb71e849d5e94401a5e0be19aef817
Leif Josh Flores <floresleifjosh@gmail.com>
2026-04-12 21:19:18 -0400

    Update Department.h

--- include/Department.h
+++ include/Department.h
@@ -4,27 +4,31 @@
 #include "Product.h"
 using namespace std;
 
-//Department class stores multiple products
+/*
+    Department class
+    This class represents the departments in the grocery store
+*/
+
 class Department {
 private:
   char departmentName[100]; //name of department
-  Product* items;
+  Product* items; //Product in the department
   int totalItems; //Number of items in department
 
 public:
   //Constructors
-     Department();
-     Department(const char* name); 
-     ~Department();
+     Department(); //Default (empty)
+     Department(const char* name);  //Innitializes the deparments
+  //Destructor
+     ~Department(); //Used to free the dynamically allocated items in the array
 
-     void setDepartmentName(const char* name);
+     void setDepartmentName(const char* name); //Sends and get department name
      const char* getDepartmentName() const;
 
-     int getTotalItems() const;
-     Product* getItems() const;
+     int getTotalItems() const; //Gets total number items in this department
+     Product* getItems() const; //get pointer to items array
 
-     void addItem(const Product& p);
-     void listItems() const;
-};
+     void addItem(const Product& p); //Adds a new product to the department
+     void listItems() const; //Displays all items in department
 
 #endif
