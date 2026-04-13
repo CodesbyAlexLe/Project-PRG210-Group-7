def37f5d3e0133e134d619210d736dcabe47afcc
Jasraj Chhokar <jchhokar2@myseneca.ca>
2026-04-12 23:21:35 -0400

    Update Department.h

--- include/Department.h
+++ include/Department.h
@@ -8,41 +8,29 @@ using namespace std;
     Department class
     This class represents the departments in the grocery store
 */
-
 class Department {
 private:
-  char departmentName[100]; //name of department
-  Product* items; //Product in the department
-  int totalItems; //Number of items in department
-    char name[100];
-    Product* items;
-    int totalItems;
+    char departmentName[100]; //name of department
+    Product* items; //Product in the department
+    int totalItems; //Number of items in department
 
 public:
-  //Constructors
-     Department(); //Default (empty)
-     Department(const char* name);  //Innitializes the deparments
-  //Destructor
-     ~Department(); //Used to free the dynamically allocated items in the array
+    //Constructors
+    Department(); //Default (empty)
+    Department(const char* name); //Innitializes the deparments
+    //Destructor
+    ~Department(); //Used to free the dynamically allocated items in the array
 
-     void setDepartmentName(const char* name); //Sends and get department name
-     const char* getDepartmentName() const;
+    void setDepartmentName(const char* name); //Sends and get department name
+    const char* getDepartmentName() const;
 
-     int getTotalItems() const; //Gets total number items in this department
-     Product* getItems() const; //get pointer to items array
+    int getTotalItems() const; //Gets total number items in this department
+    Product* getItems() const; //get pointer to items array
 
-     void addItem(const Product& p); //Adds a new product to the department
-     void listItems() const; //Displays all items in department
-    Department();
-    Department(const char* deptName);
-    ~Department();
-
-    void addItem(const Product& product);
-    void listItem() const;
-    const char* getName() const;
-    int getTotalItems() const;
-    Products& getItem(int index);
-
+    void addItem(const Product& p); //Adds a new product to the department
+    void listItems() const; //Displays all items in department
 };
+
+#endif
 
 #endif
