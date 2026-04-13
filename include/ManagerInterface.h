7975e5f0e90a96899f68b558476354d63cf12e40
Leif Josh Flores <floresleifjosh@gmail.com>
2026-04-12 21:14:38 -0400

    Update ManagerInterface.h

--- include/ManagerInterface.h
+++ include/ManagerInterface.h
@@ -4,21 +4,23 @@
 #include "Interface. h" //Including the base interface class
 
 /*
-
+    Manager interface class
+    This class represents the manager side of the program, as it inherits from the interface
+    and provides functions for managing departments and products in the grocery store
 */
 
 class ManagerInterface: public Interface 
 {
 
 public:
-  void showMenu();
-  void handleMenu();
+  void showMenu(); //Displays all the manager menu options to the user
+  void handleMenu(); //Handles input and calls the correct functions based on choice
 
 private:
-  void listDepartments();
-  void addDepartment();
-  void addItemToDepartment();
-  void saveChangesToCSV();
+  void listDepartments(); // Displays all available departments in the store
+  void addDepartment(); //prompts user to add a new department and adds it to the system
+  void addItemToDepartment(); //Allows manager to add a new item to a selected department
+  void saveChangesToCSV(); //Saves all departments and items to a csv file
 };
 
 #endif
