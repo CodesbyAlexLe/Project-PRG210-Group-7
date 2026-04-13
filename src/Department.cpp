6a45746954b39ba1d5792f460cd5f5af03fccc43
Jasraj Chhokar <jchhokar2@myseneca.ca>
2026-04-12 20:55:47 -0400

    Update Department.cpp
    Fix missing semicolon and typo in listItems loop

--- src/Department.cpp
+++ src/Department.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+// ADDEDED FOR WINDOWS USER
 #include <iostream>
 #include <cstring>
 #include "Department.h"
@@ -5,70 +7,73 @@ using namespace std;
 
 Department::Department()
 {
-  departmentName[0] = '\0';
-  items = nullptr;
-  totalItems = 0;
+    departmentName[0] = '\0';
+    items = nullptr;
+    totalItems = 0;
 }
 
 Department::Department(const char* name)
 {
-  strcpy(departmentName, name);
-  items = nullptr;
-  totalItems = 0;
+    strncpy(departmentName, name, 99);
+    departmentName[99] = '\0';
+    items = nullptr;
+    totalItems = 0;
 }
 
 Department::~Department()
 {
-delete[] items;
+    delete[] items;
 }
 
 void Department::setDepartmentName(const char* name)
 {
-    strcpy(departmentName, name);
+    strncpy(departmentName, name, 99);
+    departmentName[99] = '\0';
 }
 
 const char* Department::getDepartmentName() const
 {
-  return departmentName;
+    return departmentName;
 }
 
 int Department::getTotalItems() const
 {
-  return totalItems;
+    return totalItems;
 }
 
 Product* Department::getItems() const
 {
-  return items;
+    return items;
 }
 
 void Department::addItem(const Product& p)
 {
-  Product* temp = new Product[totalItems + 1];
+    Product* temp = new Product[totalItems + 1];
 
-  for (int i = 0; i < totalItems; i++)
-  {
-    temp[i] = items[i]
-  }
-  temp[totalItems] = p;
+    for (int i = 0; i < totalItems; i++)
+    {
+        temp[i] = items[i];
+    }
+    temp[totalItems] = p;
 
-  delete[] items;
-  items = temp;
-  totalItems++;
+    delete[] items;
+    items = temp;
+    totalItems++;
 }
 
 void Department::listItems() const
 {
-  if (totalItems == 0)
-{
-  cout << "No items in this department. " << endl;
-  return;
+    if (totalItems == 0)
+    {
+        cout << "No items in this department." << endl;
+        return;
+    }
+    for (int i = 0; i < totalItems; i++)
+    {
+        cout << i + 1 << ". "
+             << items[i].getName()
+             << " - $" << items[i].getPrice()
+             << " - Qty: " << items[i].getQuanitity() << endl;
+    }
 }
-for (int i = 0; i < totalItems; i++)
-{
-  cout << i + 1 << ". "
-       << items[i].getName()
-       << "- $" << items[i].getPrice()
-       << "- Qty:" << items[i].getQuantity() << endl;
-}
-}
+
