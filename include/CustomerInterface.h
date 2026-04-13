ec0344cba87b6e6f1ce41241801fbc1f1247908f
Alex Le <alexle16@hotmail.com>
2026-04-12 21:20:40 -0400

    Enhance CustomerInterface.h with comments
    Added comments to clarify the purpose of the class and its methods.

--- include/CustomerInterface.h
+++ include/CustomerInterface.h
@@ -1,19 +1,27 @@
 #ifndef CUSTOMERINTERFACE_H
 #define CUSTOMERINTERFACE_H
+// Include gaurd prevents this header from being included multiple times
 #include "Interface.h"
 #include "Cart.h"
 
+// CustomerInterface manages the customer side of the store system
+// It allows the user to view menus, browse departments, and use a cart
 class CustomerInterface: public Interface 
 {
 private:
+// Stores the customers shopping cart
   Cart cart;
 
 public:
+// Display the main customer menu
   void showMenu();
+// Handles the main customer menu logic
   void handleMenu();
 
 private:
+// Displays and handles the cart submenu
   void showCartMenu();
+// Lets the customer browse departments and add items to the cart
   void browseDepartments();
 
 };
