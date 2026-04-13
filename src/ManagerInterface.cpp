93469425428408e68901a41718da795908af7548
Jasraj Chhokar <jchhokar2@myseneca.ca>
2026-04-12 21:04:29 -0400

    Update ManagerInterface.cpp
    Implement manager menu with add department

--- src/ManagerInterface.cpp
+++ src/ManagerInterface.cpp
@@ -1,148 +1,169 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <fstream>
+#include <cstring>
 #include "ManagerInterface.h"
-#include "Department.h"
-#include "Product.h"
 using namespace std;
-
 extern Department* StoreDepartments;
 extern int TotalDepartments;
 extern const char* csvFile;
 
-void savetoCSV();
-
 void ManagerInterface::showMenu()
 {
-  cout << "\nManager Menu:" << endl;
-  cout << "1. List Departments" << endl;
-  cout << "2. Add Department" << endl;
-  cout << "3. Add Item to Department" << endl;
-  cout << "4. Save Changes to CSV" << endl;
-  cout << "Enter your choice [1, 2, 3, 4, 5]: " << endl;
+    cout << "\nManager Menu:" << endl;
+    cout << "1. List Departments" << endl;
+    cout << "2. Add Department" << endl;
+    cout << "3. Add Item to Department" << endl;
+    cout << "4. Save Changes to CSV" << endl;
+    cout << "5. Exit" << endl;
+    cout << "Enter your choice [1, 2, 3, 4, 5]: ";
 }
 
 void ManagerInterface::handleMenu()
 {
-  int choice;
+    int choice;
 
-  do
-{
-  showMenu();
-  choice = getValidatedInt(1, 5);
+    do
+    {
+        showMenu();
+        choice = getValidatedInt(1, 5);
 
-  switch (choice)
-{
-case 1:
-  listDepartments();
-  break;
-case 2:
-  addDepartment();
-  break;
-case 3:
-  addItemToDepartment();
-  break;
-case 4:
-  saveChangesToCSV();
-  break;
-case 5:
-  cout << "Exiting manager interface..." << endl;
-}
-} while (choice != 5);
+        switch (choice)
+        {
+        case 1:
+            listDepartments();
+            break;
+        case 2:
+            addDepartment();
+            break;
+        case 3:
+            addItemToDepartment();
+            break;
+        case 4:
+            saveToCSV();
+            break;
+        case 5:
+            cout << "Exiting manager interface..." << endl;
+            break;
+        }
+    } while (choice != 5);
 }
 
 void ManagerInterface::listDepartments()
 {
-  if (TotalDepartments == 0)
-{
-  cout << "No departments available. " << endl;
-  return;
-}
+    if (TotalDepartments == 0)
+    {
+        cout << "No departments available." << endl;
+        return;
+    }
 
-for (int i = 0; i < TotalDepartments; i++)
-{
-  cout << i + 1 << ". " << StoreDepartments[i].getDepartmentName() << endl;
-}
+    for (int i = 0; i < TotalDepartments; i++)
+    {
+        cout << i + 1 << ". " << StoreDepartments[i].getDepartmentName() << endl;
+    }
 }
 
 void ManagerInterface::addDepartment()
 {
-  char name[100];
-  cout << "Enter department name: ";
-  getValidatedNonEmptyString(name, 100);
+    char name[100];
+    cout << "Enter department name: ";
+    // getValidatedInt already cleared the newline, so getline works directly
+    getValidatedNonEmptyString(name, 100);
 
-Department* newDepartments = new Department[TotalDepartments + 1];
+    Department* newDepts = new Department[TotalDepartments + 1];
+    for (int i = 0; i < TotalDepartments; i++)
+    {
+        newDepts[i] = StoreDepartments[i];
+    }
+    newDepts[TotalDepartments] = Department(name);
+    delete[] StoreDepartments;
+    StoreDepartments = newDepts;
+    TotalDepartments++;
 
-for (int i = 0; i < TotalDepartments; i++)
-{
-  newDepartments[i] = StoreDepartments[i];
-}
-
-newDepartments[TotalDepartments] = Department(name);
-delete[] StoreDepartments;
-StoreDepartments = newDepartments;
-TotalDepartments++;
-
-cout << "Department added successfully. " << endl;
+    cout << "Department added successfully." << endl;
 }
 
 void ManagerInterface::addItemToDepartment()
 {
-  if (TotalDepartments == 0)
-{
-  cout << "No departments available. " << endl;
-  return;
+    if (TotalDepartments == 0)
+    {
+        cout << "No departments available." << endl;
+        return;
+    }
+
+    for (int i = 0; i < TotalDepartments; i++)
+    {
+        cout << i + 1 << ". " << StoreDepartments[i].getDepartmentName() << endl;
+    }
+
+    cout << "Enter department number [0 to go back]: ";
+
+    int dep;
+    while (true)
+    {
+        cin >> dep;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Enter department number [0 to go back]: ";
+        }
+        else if (dep == 0)
+        {
+            cin.ignore(1000, '\n');
+            return;
+        }
+        else if (dep < 1 || dep > TotalDepartments)
+        {
+            cin.ignore(1000, '\n');
+            cout << "Enter department number [0 to go back]: ";
+        }
+        else
+        {
+            cin.ignore(1000, '\n');
+            break;
+        }
+    }
+
+    StoreDepartments[dep - 1].listItems();
+
+    char itemName[100];
+    cout << "Enter item name: ";
+    // after cin >> dep + cin.ignore, buffer is clean for getline
+    getValidatedNonEmptyString(itemName, 100);
+
+    cout << "Enter item price: ";
+    double price = getValidatedPositiveDouble();
+
+    cout << "Enter item quantity: ";
+    int qty = getValidatedPositiveInt();
+
+    Product p(itemName, price, qty);
+    StoreDepartments[dep - 1].addItem(p);
+
+    cout << "Item added successfully." << endl;
 }
 
-listDepartments();
-cout << "Enter a department number [0 to go back]: ";
-
-int deptChoice;
-
-while (true)
+void ManagerInterface::saveToCSV()
 {
-  cin >> deptChoice;
-  if (cin.fail())
-{
-  cin.clear();
-  cin.ignore(1000, '\n');
-  cout << "Invalid input. Enter department number [0 to go back]: ";
-}
-else if (deptChoice == 0)
-{
-  cin.ignore(1000, '\n')
-  return;
-}
-else if (deptChoice < 1 || deptChoice > TotalDepartments)
-{
-  cin.ignore(1000, '\n')
-  cout << "Invalid department number. Enter department number [0 to go back]: ";
-}
-else
-{
-  cin.ignore(1000, '\n');
-  break;
-}
-}
-char itemName[100];
-double price;
-int quantity;
+    ofstream file(csvFile);
 
-cout << "Enter item name: ";
-getValidatedNonEmptyString(itemName, 100);
+    file << TotalDepartments << endl;
 
-cout << "Enter item price: ";
-price = getValidatedPositiveDouble();
+    for (int i = 0; i < TotalDepartments; i++)
+    {
+        file << StoreDepartments[i].getDepartmentName() << ","
+             << StoreDepartments[i].getTotalItems() << endl;
 
-cout << "Enter item quantity: ";
-quantity = getValidatedPositiveInt();
+        Product* list = StoreDepartments[i].getItems();
+        for (int j = 0; j < StoreDepartments[i].getTotalItems(); j++)
+        {
+            file << list[j].getName() << ","
+                 << list[j].getPrice() << ","
+                 << list[j].getQuanitity() << endl;
+        }
+    }
 
-Product newProduct(itemName, price, quantity);
-StoreDepartments[deptChoice - 1].addItem(newProduct);
-
-cout << "Item added successfully. " << endl;
-}
-
-void ManagerInterface::saveChangesToCSV()
-{
-  saveToCSV();
-  cout << "Changes saved successfully. " << endl;
+    file.close();
+    cout << "Changes saved successfully." << endl;
 }
