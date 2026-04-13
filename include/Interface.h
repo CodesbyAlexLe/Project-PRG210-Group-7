1c03b6e8f3197b99d7f4f7e74661a59c36ba290b
Leif Josh Flores <floresleifjosh@gmail.com>
2026-04-12 21:16:41 -0400

    Update Interface.h

--- include/Interface.h
+++ include/Interface.h
@@ -7,15 +7,17 @@
 /*
     interface base class for both manager and customer interfaces
     demonstrates inheritance + polymorphism
+        - Inheritance (Manager interface and customer interface inherit from this)
+        - Polymorphism 
  */
 class Interface {
 public:
-    virtual void showMenu() = 0;
-    virtual void handleMenu() = 0;
-    virtual ~Interface();
+    virtual void showMenu() = 0; //Displays the menu
+    virtual void handleMenu() = 0; // handles users' input and performs actions based on menu choice
+    virtual ~Interface(); //Destructor 
 
 protected:
-    int getValidatedInt(int min, int max);
+    int getValidatedInt(int min, int max); //Validates integer input within a given range 
     int getValidatedPositiveInt();
     double getValidatedPositiveDouble();
     void getValidatedNonEmptyString(char* str, int size);
